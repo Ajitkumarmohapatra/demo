d58ee134bfab40207f0a7756602d6cfc60be913d
Ajitkumarmohapatra <167737649+Ajitkumarmohapatra@users.noreply.github.com>
2024-04-22 21:08:58 +0530

    Add files via upload

---
+++ thow.java
@@ -0,0 +1,24 @@
+/*class thow{
+    void devide()
+    {
+       try
+       {
+        int a = 100,b=0,c;
+        c=a/b;
+        System.out.println(c);
+       }
+       catch(Exception e)
+       {
+        e.printStackTrace();
+       }
+    }
+    public static void main(String []args)
+    {
+        thow obj = new thow();
+        obj.devide();
+        System.out.println("checking for Abnormal termination");
+
+    }
+
+}
+*/
